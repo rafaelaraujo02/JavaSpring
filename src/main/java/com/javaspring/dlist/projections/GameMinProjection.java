fd696bc22a66b7362f9a782483412cd1bc1b5813
Rafael <rafaelaraujo.02@hotmail.com>
2023-07-20 10:46:04 -0300

    Homologacao

--- src/main/java/com/javaspring/dlist/projections/GameMinProjection.java
+++ src/main/java/com/javaspring/dlist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.javaspring.dlist.projections;
 public interface GameMinProjection {
 	Long getId();
 	String getTitle();
-	Integer getYear();
+	Integer getGameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
